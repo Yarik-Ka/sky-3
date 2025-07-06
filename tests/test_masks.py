caa61e8c76204571bda4234819cf2e2f998bf75b
mac <mac@MacBook-Air-mac.local>
2025-07-07 02:56:27 +0300

    Обновление тестов маскировки

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,23 +1,16 @@
-
-
 import pytest
-from src import masks
+from masks import get_mask_account, get_mask_card_number
 
+@pytest.mark.parametrize("account_number, expected", [
+    ("1234567890", "******7890"),
+    ("9876543210", "******3210"),
+])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
 
-def test_get_mask_card_number():
-    assert masks.get_mask_card_number('1234 5678 9012 3456') == '**** **** **** 3456'
-    assert masks.get_mask_card_number('1234567890123456') == '**** **** **** 3456'
-
-
-def test_get_mask_account():
-    assert masks.get_mask_account('1234567890') == '******7890'
-
-
-def test_short_card():
-    with pytest.raises(ValueError):
-        masks.get_mask_card_number('123')
-
-
-def test_short_account():
-    with pytest.raises(ValueError):
-        masks.get_mask_account('1')
+@pytest.mark.parametrize("card_number, expected", [
+    ("1234 5678 9012 3456", "**** **** **** 3456"),
+    ("1234567890123456", "**** **** **** 3456"),
+])
+def test_get_mask_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
